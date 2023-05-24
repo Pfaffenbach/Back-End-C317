a8678bec2e98dc5375991fe2f95857237ad764d4
Lucas Piva <lucas.piva@gec.inatel.br>
2023-05-23 23:08:56 -0300

    fix: corrigindo endpoints

--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://user:user@cluster0.ff37fpa.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://user:user@cluster0.lhcebkf.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectToDatabase = async () => {
   await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
   });
 };
 
@@ -20,23 +19,23 @@ const FuncSchema = new Schema({
   id: {
     type: String,
     index: true,
-    unique: true
+    unique: true,
   },
   email: {
     type: String,
-    unique: true
+    unique: true,
   },
   nome: String,
   senha: String,
   servico: String,
   celular: String,
-  cidade: String
+  cidade: String,
 });
 
-const FuncModel = mongoose.model('FuncModel', FuncSchema);
+const FuncModel = mongoose.model("FuncModel", FuncSchema);
 
 module.exports = {
   FuncModel,
   connectToDatabase,
-  disconnectFromDatabase
+  disconnectFromDatabase,
 };
--- src/port/func_repository.js
+++ src/port/func_repository.js
@@ -1,57 +1,96 @@
-const { FuncModel } = require('../infrastructure/database');
+const jsonwebtoken = require("jsonwebtoken");
+
+const { FuncModel, connectToDatabase } = require("../infrastructure/database");
 
 const FuncRepository = {
-    async create(data) {
-        try {
-            const model = new FuncModel(data);
-            const response = await model.save();
-            return response.toObject();
-        } catch (e) {
-            return e;
-        }
-    },
+  async create(data) {
+    try {
+      await connectToDatabase();
+      const model = new FuncModel(data);
+      const response = await model.save();
+      return response.toObject();
+    } catch (e) {
+      return e;
+    }
+  },
 
-    async update(data) {
-        try {
-            const update = {
-                nome: data.nome,
-            };
-            const options = { new: true };
-            const filter = { email: data.email };
-            const result = await FuncModel.findOneAndUpdate(filter, update, options).exec();
-            if (result === null) return []
-            return result.toObject();
-        } catch (e) {
-            return e;
-        }
-    },
+  async update(data) {
+    try {
+      await connectToDatabase();
+      const update = {
+        nome: data.nome,
+        email: data.email,
+        servico: data.servico,
+        celular: data.celular,
+        cidade: data.cidade,
+      };
+      const options = { new: true };
+      const filter = { id: data.id };
+      const result = await FuncModel.findOneAndUpdate(
+        filter,
+        update,
+        options
+      ).exec();
+      if (result === null) return [];
+      return result.toObject();
+    } catch (e) {
+      return e;
+    }
+  },
 
-    async list() {
-        try {
-            const result = await FuncModel.find().exec();
-            return result;
-        } catch (error) {
-            return error;
-        }
-    },
+  async list() {
+    try {
+      await connectToDatabase();
+      const result = await FuncModel.find();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  },
 
-    async getByJob(data) {
-        try {
-            const result = await FuncModel.findOne({ job: data.job }).exec();
-            return result;
-        } catch (e) {
-            return e;
-        }
-    },
+  async getByJob(data) {
+    try {
+      const result = await FuncModel.findOne({ job: data.job }).exec();
+      return result;
+    } catch (e) {
+      return e;
+    }
+  },
 
-    async delete(data) {
-        try {
-            const result = await FuncModel.deleteOne({ email: data.email }).exec();
-            return result.deletedCount;
-        } catch (error) {
-            return error;
-        }
-    },
+  async delete(data) {
+    try {
+      await connectToDatabase();
+      const result = await FuncModel.deleteOne({ email: data.email }).exec();
+      return result.deletedCount;
+    } catch (error) {
+      return error;
+    }
+  },
+
+  async login(data) {
+    try {
+      await connectToDatabase();
+      console.log("aqui");
+
+      const user = await FuncModel.findOne({
+        email: data.email,
+        senha: data.senha,
+      });
+
+      console.log(user);
+
+      if (!user) return { erro: "E-mail ou senha incorretos!" };
+
+      const token = await jsonwebtoken.sign(
+        {
+          email: user.email,
+        },
+        "getjobs"
+      );
+
+      return token;
+    } catch (error) {}
+  },
 };
 
 module.exports = FuncRepository;
